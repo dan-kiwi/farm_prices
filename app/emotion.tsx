05786225636f24579be2fdf20feaf38552e372bc
Daniel Bishop <career@dan-kiwi.dev>
2023-07-05 09:51:03 +0100

    Prettier and ESLint install

--- app/emotion.tsx
+++ app/emotion.tsx
@@ -5,27 +5,27 @@ import { useEmotionCache, MantineProvider } from "@mantine/core";
 import { useServerInsertedHTML } from "next/navigation";
 
 export default function RootStyleRegistry({
-                                              children,
-                                          }: {
-    children: React.ReactNode;
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    const cache = useEmotionCache();
-    cache.compat = true;
+  const cache = useEmotionCache();
+  cache.compat = true;
 
-    useServerInsertedHTML(() => (
-        <style
-            data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(" ")}`}
-            dangerouslySetInnerHTML={{
-                __html: Object.values(cache.inserted).join(" "),
-            }}
-        />
-    ));
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(" ")}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(" "),
+      }}
+    />
+  ));
 
-    return (
-        <CacheProvider value={cache}>
-            <MantineProvider withGlobalStyles withNormalizeCSS>
-                {children}
-            </MantineProvider>
-        </CacheProvider>
-    );
+  return (
+    <CacheProvider value={cache}>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        {children}
+      </MantineProvider>
+    </CacheProvider>
+  );
 }
