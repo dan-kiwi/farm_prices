ed43b649530e822f2d61df7f658ee309333c4651
Daniel Bishop <career@dan-kiwi.dev>
2023-09-08 16:29:27 +1200

    Revert "Changed priceInfo to server component"
    This reverts commit f7d41f5b0c1d423c10dd798904ab9f3a2293d8fe.

--- app/(prices)/PriceInfo.tsx
+++ app/(prices)/PriceInfo.tsx
@@ -1,13 +1,17 @@
+"use client";
+
+// import { Button, Group, HoverCard, Text } from "@mantine/core";
+// import { IconInfoCircle } from "@tabler/icons-react";
 import { store } from "@/store";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState, useEffect, useMemo } from "react";
 import {
   itemVarietiesMaster,
   itemsMaster,
   regionsMaster,
 } from "@/types/itemRegionMaster";
-import { cookies } from "next/headers";
 
-export default async function PriceInfo() {
+export default function PriceInfo() {
   const userPricePreferences = store.getState().userPricePreferences;
   const region = regionsMaster[userPricePreferences.region];
   const variety =
@@ -15,16 +19,26 @@ export default async function PriceInfo() {
       userPricePreferences.variety
     ];
 
-  const supabase = createServerComponentClient({ cookies });
+  const supabase = createClientComponentClient();
+  const [currentPrice, setCurrentPrice] = useState<number>(400);
   const currentPriceId =
     userPricePreferences.region * 200 ** 2 +
     userPricePreferences.item * 200 +
     userPricePreferences.variety;
-  const { data, error } = await supabase
-    .from("prices_current")
-    .select("price")
-    .eq("region_item_variety", currentPriceId);
-  const currentPrice = data ? data[0].price : null;
+  useEffect(() => {
+    const fetchCurrentPrice = async () => {
+      let { data, error } = await supabase
+        .from("prices_current")
+        .select("price")
+        .eq("region_item_variety", currentPriceId);
+      if (error || !data || data.length !== 1) {
+        console.log(error);
+      } else {
+        setCurrentPrice(data[0].price);
+      }
+    };
+    fetchCurrentPrice();
+  }, [supabase, currentPriceId]);
 
   return (
     <div className="flex flex-row justify-between items-center">
@@ -32,6 +46,21 @@ export default async function PriceInfo() {
         <h1>
           {variety}, {region}: ${currentPrice ?? "???"}
         </h1>
+        <div className="mb-2.5">
+          {/*<Group>*/}
+          {/*  <HoverCard width={250} shadow="md">*/}
+          {/*    <HoverCard.Target>*/}
+          {/*      <IconInfoCircle size="1rem" />*/}
+          {/*    </HoverCard.Target>*/}
+          {/*    <HoverCard.Dropdown>*/}
+          {/*      <Text size="sm">*/}
+          {/*        This is our estimate price based on our current data. We do*/}
+          {/*        not guarantee the accuracy of this price.*/}
+          {/*      </Text>*/}
+          {/*    </HoverCard.Dropdown>*/}
+          {/*  </HoverCard>*/}
+          {/*</Group>*/}
+        </div>
       </div>
       {/*<Button>Change This</Button>*/}
     </div>
